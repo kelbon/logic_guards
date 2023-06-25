8f73dd68d307a219868d9b6a208edeb91b61737d
Kelbon Nik <kelbonage@gmail.com>
2023-06-25 17:26:45 +0400

    initial

---
+++ include/logic_guards/logic_guards.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#if __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-attributes"
+#endif
+
+#include <type_traits>
+#include <utility>
+
+#if __cplusplus >= 202002L
+#define LOGIC_GUARDS_CONSTEXPR_DCTOR constexpr
+#else
+#define LOGIC_GUARDS_CONSTEXPR_DCTOR
+#endif
+
+namespace aa {
+
+// SCOPE EXIT
+// invokes 'Foo' when destroyed
+// if scope ended with exception and 'Foo' throws exception terminate is called
+template <typename Foo>
+struct [[nodiscard("name it")]] scope_exit {
+  [[no_unique_address]] Foo fn;
+
+  LOGIC_GUARDS_CONSTEXPR_DCTOR
+  ~scope_exit() noexcept(noexcept(fn())) {
+    fn();
+  }
+};
+template <typename T>
+scope_exit(T&&) -> scope_exit<std::decay_t<T>>;
+
+// SCOPE FAILURE
+// invokes 'Foo' when destroyed and success point not reached
+// success point setted by invoking .success_point()
+template <typename Foo>
+struct [[nodiscard("name it and set success point")]] on_scope_failure {
+  [[no_unique_address]] Foo fn;
+  bool failed = true;
+
+  template <typename... Args>
+  constexpr on_scope_failure(Args && ... args) : failed(true), fn(std::forward<Args>(args)...) {
+  }
+
+  template <typename T>
+  void operator=(T&&) = delete;
+
+  LOGIC_GUARDS_CONSTEXPR_DCTOR
+  ~on_scope_failure() noexcept(noexcept(fn())) {
+    if (failed)
+      fn();
+  }
+  // should be called where the 'fn' call is no longer needed
+  constexpr void success_point() noexcept {
+    failed = false;
+  }
+};
+template <typename T>
+on_scope_failure(T&&) -> on_scope_failure<std::decay_t<T>>;
+
+}  // namespace aa
+
+namespace aa::noexport {
+
+// used only in macro
+struct make_scope_exit {
+  template <typename T>
+  constexpr auto operator+(T&& fn) const noexcept(std::is_nothrow_constructible_v<T, T&&>) {
+    return scope_exit{std::forward<T>(fn)};
+  }
+};
+
+}  // namespace aa::noexport
+
+#define on_scope_exit                                                                  \
+  [[maybe_unused]] const auto LOGIC_GUARDS_CONCAT(scope_exit, __LINE__, __COUNTER__) = \
+      ::aa::noexport::make_scope_exit{} + [&]()
+
+#define LOGIC_GUARDS_CONCAT_IMPL_EXPAND(a, b, c) a##_##b##_##c
+#define LOGIC_GUARDS_CONCAT_IMPL(a, b, c) LOGIC_GUARDS_CONCAT_IMPL_EXPAND(a, b, c)
+#define LOGIC_GUARDS_CONCAT(a, b, c) LOGIC_GUARDS_CONCAT_IMPL(a, b, c)
+
+#if __clang__
+#pragma clang diagnostic pop
+#endif
---
+++ tests/test_logic_guards.cpp
@@ -0,0 +1,42 @@
+
+#include "logic_guards/logic_guards.hpp"
+
+#include <mutex>
+
+template <int I>
+bool b = false;
+
+template <int I>
+void f() {
+  b<I> = true;
+}
+
+int main() {
+  on_scope_exit {
+    if (!b<0> || !b<1> || !b<4> || !b<5> || b<6>)
+      exit(3);
+  };
+  on_scope_exit {
+    f<0>();
+  };
+  on_scope_exit {
+    f<1>();
+  };
+  {
+    aa::on_scope_failure set_bool = [] { f<2>(); };
+  }
+  if (!b<2>)
+    return 1;
+  {
+    aa::on_scope_failure set_bool = [] { f<3>(); };
+    set_bool.success_point();
+  }
+  if (b<3>)
+    return 2;
+  aa::scope_exit no_move {[m = std::mutex()] {
+    f<4>();
+  }};
+  aa::scope_exit from_fn{&f<5>};
+  aa::scope_exit from_fn_ref{f<6>};
+  return 0;
+}
