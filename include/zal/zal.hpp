bf050762c914883fa221f9bed7c1987afd07818a
kelbon <you@example.com>
2025-08-26 11:10:47 +0500

    handle guards move

--- include/zal/zal.hpp
+++ include/zal/zal.hpp
@@ -21,6 +21,20 @@
 #define ZAL_NO_UNIQUE_ADDRESS
 #endif
 
+namespace zal::noexport {
+// forbids move for type without breaking 'is_aggregate'
+struct pin {
+  pin() = default;
+  pin(pin&&) = delete;
+  void operator=(pin&&) = delete;
+};
+
+}  // namespace zal::noexport
+
+// must be used as field in type, makes it unmovable without breaking
+// is_aggregate
+#define ZAL_PIN ZAL_NO_UNIQUE_ADDRESS ::zal::noexport::pin _pin_ = {}
+
 // both scope_exit and scope_failure use precondition,
 // that invoking 'fn' do not dellocates memory where 'scope_exit/failure' exists
 // or atleast do not touch memory after deleting(including lambda capture/references to other objects)
@@ -33,6 +47,7 @@ namespace zal {
 template <typename Foo>
 struct [[nodiscard("name it")]] scope_exit {
   ZAL_NO_UNIQUE_ADDRESS Foo fn;
+  ZAL_PIN;
 
   LOGIC_GUARDS_CONSTEXPR_DCTOR
   ~scope_exit() noexcept(noexcept(fn())) {
@@ -42,15 +57,48 @@ struct [[nodiscard("name it")]] scope_exit {
 template <typename T>
 scope_exit(T&&) -> scope_exit<std::decay_t<T>>;
 
+struct movebool {
+  bool value;
+
+  movebool() = default;
+  movebool(bool b) noexcept : value(b) {
+  }
+  movebool(movebool const& other) = default;
+  movebool& operator=(movebool const& other) = default;
+  movebool(movebool&& other) noexcept : value(std::exchange(other.value, false)) {
+  }
+  movebool& operator=(movebool&& other) noexcept {
+    std::swap(value, other.value);
+    return *this;
+  }
+
+  operator bool() const noexcept {
+    return value;
+  }
+};
+
 // ON SCOPE FAILURE
 // invokes 'Foo' when destroyed and success point not reached
 // success point setted by invoking .no_longer_needed()
+// may be moved, guarantees `fn` will be invoked once
 template <typename Foo>
 struct [[nodiscard("name it and set success points")]] scope_failure {
   ZAL_NO_UNIQUE_ADDRESS Foo fn;
-  bool failed = true;
+  movebool failed = true;
 
-  void operator=(scope_failure&&) = delete;
+  scope_failure(Foo fn) noexcept
+    requires(std::is_nothrow_move_constructible_v<Foo>)
+      : fn(std::move(fn)) {
+  }
+  scope_failure(Foo fn)
+    requires(!std::is_nothrow_move_constructible_v<Foo>)
+  try : fn(std::move(fn)) {
+  } catch (...) {
+    fn();
+    throw;
+  }
+  scope_failure(scope_failure&&) = default;
+  scope_failure& operator=(scope_failure&&) = default;
 
   LOGIC_GUARDS_CONSTEXPR_DCTOR
   ~scope_failure() noexcept(noexcept(fn())) {
