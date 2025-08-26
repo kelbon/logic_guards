ce3923e17a0e22883b20d9d3c5e6557f9f285d70
kelbon <you@example.com>
2025-08-26 11:10:47 +0500

    handle guards move

--- tests/test_zal.cpp
+++ tests/test_zal.cpp
@@ -5,19 +5,33 @@
 #include <vector>
 #include <functional>
 
+#define REQUIRE(...)      \
+  if (!bool(__VA_ARGS__)) \
+  std::exit(-1)
+
 template <int I>
 bool b = false;
 
 template <int I>
 void f() {
+  REQUIRE(!b<I>);
   b<I> = true;
 }
 
+struct bad_type {
+  bad_type() = default;
+
+  bad_type(bad_type&&) {
+    throw std::runtime_error("bad type");
+  }
+  void operator()() {
+    f<10>();
+  }
+};
+
 int main() {
   on_scope_exit {
-    if (b<0> && b<1> && b<4> && b<5> && b<6> && b<7> && b<8>)
-      return;
-    exit(3);
+    REQUIRE(b<0> && b<1> && b<4> && b<5> && b<6> && b<7> && b<8> && b<9> && b<10>);
   };
   on_scope_exit {
     f<0>();
@@ -43,11 +57,34 @@ int main() {
   zal::scope_exit no_move{[m = std::mutex()] { f<4>(); }};
   zal::scope_exit from_fn{&f<5>};
   zal::scope_exit from_fn_ref{f<6>};
-  std::vector<zal::scope_exit<std::function<void()>>> wtf{
-      {&f<7>},
-      {f<8>},
-      {[] {}},
-  };
-  (void)wtf;
+  zal::scope_failure x([] { f<9>(); });
+  REQUIRE(x.failed);
+  auto xx = std::move(x);
+  REQUIRE(!x.failed && xx.failed);
+  std::swap(xx, x);
+  REQUIRE(!xx.failed && x.failed);
+  std::swap(xx, x);
+  REQUIRE(!x.failed && xx.failed);
+  x = std::move(xx);
+  REQUIRE(x.failed && !xx.failed);
+  std::swap(xx, x);
+  REQUIRE(!x.failed && xx.failed);
+  xx = std::move(x);
+  REQUIRE(!xx.failed && x.failed);
+  xx = std::move(xx);
+  REQUIRE(!xx.failed);
+  x = std::move(x);
+  REQUIRE(x.failed);
+
+  try {
+    zal::scope_failure abc(bad_type{});
+    REQUIRE(false);
+  } catch (std::runtime_error& e) {
+    REQUIRE(std::string_view(e.what()) == "bad type");
+  }
+  std::vector<zal::scope_failure<std::function<void()>>> wtf;
+  wtf.push_back({&f<7>});
+  wtf.push_back({f<8>});
+  wtf.push_back({[] {}});
   return 0;
 }
